281ff12ec5aa00efacdcd889361ea0430dff7d11
squall-1002 <marcel.kurovski@googlemail.com>
2019-02-12 22:22:38 +0100

    fix: Fixes agent display state error, introduce deterministic policy

--- cartpole_hill_climbing/agent.py
+++ cartpole_hill_climbing/agent.py
@@ -8,16 +8,18 @@ import torch
 
 
 class Agent(torch.nn.Module):
-    def __init__(self, state_size: int, action_size: int, hidden_size_1):
+    def __init__(self, state_size: int, action_size: int, hidden_size_1: int,
+                 stochastic: bool = True):
         super(Agent, self).__init__()
         self.actions = list(range(action_size))
         self.linear_1 = torch.nn.Linear(state_size, hidden_size_1, bias=False)
         self.linear_2 = torch.nn.Linear(hidden_size_1, action_size, bias=False)
         self.softmax = torch.nn.Softmax(dim=0)
+        self.stochastic = stochastic
 
     def _forward(self, state: np.ndarray):
         x = torch.from_numpy(state).float()
-        x = torch.relu(self.linear_1(x))
+        x = self.linear_1(x)
         x = self.linear_2(x)
 
         return self.softmax(x)
@@ -25,7 +27,10 @@ class Agent(torch.nn.Module):
     def get_action(self, state):
         action_probabilities = self._forward(state).detach().numpy()
         # sample an action according to the softmax probabilities (stochastic policy)
-        action = np.random.choice(self.actions, size=1, p=action_probabilities)[0]
+        if self.stochastic:
+            action = np.random.choice(self.actions, size=1, p=action_probabilities)[0]
+        else:
+            action = np.argmax(action_probabilities)
 
         return action
 
@@ -47,8 +52,9 @@ class Agent(torch.nn.Module):
             env.render()
             action = self.get_action(state)
             next_state, reward, done, _ = env.step(action)
+            state = next_state
             if done:
-                env.reset()
+                state = env.reset()
 
         env.close()
 
